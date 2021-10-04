633d5f813dba04643f527d229950d1d0af4dee8c
Carl Powaza <carlpowaza@gmail.com>
2021-10-03 20:51:11 -0500

    Initial commit

---
+++ src/test/java/edu/neiu/mininotes/MininotesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.mininotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MininotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
