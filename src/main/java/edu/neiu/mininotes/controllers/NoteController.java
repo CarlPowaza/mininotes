f2a6d22b08b7d161064f73e0827a459a7a9df9ac
Carl Powaza <carlpowaza@gmail.com>
2021-10-12 16:46:09 -0500

    Got the form done and added postmapping

--- src/main/java/edu/neiu/mininotes/controllers/NoteController.java
+++ src/main/java/edu/neiu/mininotes/controllers/NoteController.java
@@ -5,27 +5,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/note")
-
 public class NoteController {
 
 
     @GetMapping
     public String getNote(Model model){
-          model.addAttribute("note",new note());
+        model.addAttribute("note",new note());
         return "add-page";
     }
 
-    @ModelAttribute
-    public void addNotes(Model model){
-        ArrayList<note> myNotes = new ArrayList<note>();
-        myNotes.add(new note("testing","is it working"));
-        model.addAttribute("notes",myNotes);
-
+//    @ModelAttribute
+//    public void addNotes(Model model){
+//        ArrayList<note> myNotes = new ArrayList<note>();
+//        myNotes.add(new note("testing","is it working"));
+//        model.addAttribute("notes",myNotes);
+//
+//    }
+    @PostMapping
+    public String handleNoteForm(@ModelAttribute("note") note note){
+        System.out.println(note.getTitle()+" "+note.getBody());
+        return "redirect:/";
     }
+
+
+
+
 }
