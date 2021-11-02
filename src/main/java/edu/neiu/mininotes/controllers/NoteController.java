e3936e172da54a39f78a907b263dee7ba65d367b
Carl Powaza <carlpowaza@gmail.com>
2021-11-02 18:37:32 -0500

    notes now save to DB

--- src/main/java/edu/neiu/mininotes/controllers/NoteController.java
+++ src/main/java/edu/neiu/mininotes/controllers/NoteController.java
@@ -1,6 +1,8 @@
 package edu.neiu.mininotes.controllers;
 
+import edu.neiu.mininotes.data.NoteRepository;
 import edu.neiu.mininotes.models.Note;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +16,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class NoteController {
 
 
+    private NoteRepository noteRepo;
+
+    @Autowired
+    public NoteController(NoteRepository noteRepo){
+        this.noteRepo  = noteRepo;
+    }
+
     @GetMapping
     public String getNote(Model model){
         model.addAttribute("note",new Note());
         return "add-page";
     }
 
-//    @ModelAttribute
-//    public void addNotes(Model model){
-//        ArrayList<note> myNotes = new ArrayList<note>();
-//        myNotes.add(new note("testing","is it working"));
-//        model.addAttribute("notes",myNotes);
-//
-//    }
     @PostMapping
     public String handleNoteForm(@ModelAttribute("note") Note note){
-        System.out.println(note.getTitle()+" "+note.getBody()+" "+note.getDate());
-      //  atr.addFlashAttribute("wholNote",note.getTitle() + " "+note.getBody());
-
+        this.noteRepo.save(note);
        return "redirect:/";
     }
 
